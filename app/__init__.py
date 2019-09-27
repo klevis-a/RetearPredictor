806720687e7e20baf25c79bdaa01826314c102d8
Klevis <klevis.aliaj@utah.edu>
2019-09-26 22:23:31 -0600

    Environment variables related to database now just stay as environment variables instead of becoming local variables.

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-db_host = os.environ['DB_HOST']
-db_user = os.environ['DB_USER']
-db_pass = os.environ['DB_PASS']
-db_port = os.environ['DB_PORT']
-db_name = os.environ['DB_NAME']
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + db_user + ':' + db_pass + '@' + db_host + ':' + db_port \
-                                        + '/' + db_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.environ['DB_USER'] + ':' + os.environ['DB_PASS'] + \
+                                        '@' + os.environ['DB_HOST'] + ':' + os.environ['DB_PORT'] + \
+                                        '/' + os.environ['DB_NAME']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
